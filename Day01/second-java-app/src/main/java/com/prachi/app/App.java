c6becef3af9703d58f4426372696c7d73aead2f5
Prachi <apale.prachi@tcs.com>
2023-01-13 05:24:12 +0000

    first spring boot app

--- Day01/second-java-app/src/main/java/com/prachi/app/App.java
+++ Day01/second-java-app/src/main/java/com/prachi/app/App.java
@@ -9,12 +9,16 @@ public class App
     public static void main( String[] args )
     {
         Greeting greeting = new Greeting();
+
         String result= greeting.SayHi();
         System.out.println(result);
+
         String result1= greeting.SayWelcome();
         System.out.println(result1);
+
         String result2= greeting.SayHello();
         System.out.println(result2);
+
         String result3= greeting.SayBye();
         System.out.println(result3);
 
--- Day01/second-java-app/src/main/java/com/prachi/app/Greeting.java
+++ Day01/second-java-app/src/main/java/com/prachi/app/Greeting.java
@@ -5,9 +5,13 @@ public class Greeting {
     public String SayHi(){
         return "Hi from greeting";
     }
-    public String SayWelcome(){ return "Welcome from greeting";}
-
-    public String SayHello(){ return "Hello from greeting";}
-
-    public String SayBye(){ return "Bye from greeting";}
+    public String SayWelcome(){
+        return "Welcome from greeting";
+    }
+    public String SayHello(){
+        return "Hello from greeting";
+    }
+    public String SayBye(){
+        return "Bye from greeting";
+    }
 }
